feb4e3279cf739cdb08803916952aeb193e4e994
Ramcharanraj <Ramcharanraj94@gmail.com>
2021-10-16 01:24:48 +0530

    ADDED:Welcome to Lamda Expressions

---
+++ Lamda/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lamda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

60ee75c40f3c8a681a9ae157f3f131b4a168def4
Ramcharanraj <Ramcharanraj94@gmail.com>
2021-10-16 04:35:41 +0530

    ADDED: UC-1

---
+++ Lamda/Lamdapra.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lamda
+{
+    public class Lamdapra
+    {
+        public string SSN;
+        public string Name;
+        public string Address;
+        public int Age;
+
+
+        public Lamdapra(string ssn, string name, string addr, string v, int age)
+        {
+            SSN = ssn;
+            Name = name;
+            Address = addr;
+            Age = age;
+        }
+    }
+}
--- Lamda/Program.cs
+++ Lamda/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lamda
 {
@@ -6,7 +7,27 @@ namespace Lamda
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Lamdapra> listPersonCity = new List<Lamdapra>();
+            AddRecords(listPersonCity);
+
+            
+
+        }
+
+       
+
+        //UC 1
+        private static void AddRecords(List<Lamdapra> listPersoncity)
+        {
+            listPersoncity.Add(new Lamdapra("202324093", "john", "12 Main Statement", "NewYork", 15));
+            listPersoncity.Add(new Lamdapra("202324094", "SAM", "13 Main Statement", "NewYork", 25));
+            listPersoncity.Add(new Lamdapra("202324095", "Elan", "14 Main Statement", "NewYork", 35));
+            listPersoncity.Add(new Lamdapra("202324096", "Smith", "15 Main Statement", "NewYork", 45));
+            listPersoncity.Add(new Lamdapra("202324097", "SAM", "16 Main Statement", "NewYork", 55));
+            listPersoncity.Add(new Lamdapra("202324098", "Sue", "19 Main Statement", "NewYork", 65));
+            listPersoncity.Add(new Lamdapra("202324098", "Winston", "89 Main Statement", "NewYork", 65));
+            listPersoncity.Add(new Lamdapra("202324098", "Mac", "89 Main Statement", "NewYork", 85));
+            listPersoncity.Add(new Lamdapra("202324098", "Sam", "89 Main Statement", "NewYork", 95));
         }
     }
 }
